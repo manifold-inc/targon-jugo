e89f27364a60df8d470d0c4b6dfca72919e6bb62
darwich6 <adarwich2010@gmail.com>
2024-08-26 14:39:52 -0500

    fix: weird removals

--- ingestor.py
+++ ingestor.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
-from typing import List, Dict, Any
+from typing import List, Optional, Dict, Any
 import time
 import asyncpg
 from dotenv import load_dotenv
@@ -40,7 +40,7 @@ async def is_authorized_hotkey(conn, signed_by: str) -> bool:
     return row is not None
 
 
-# Ingest endpoint
+# MinerResponse endpoint
 @app.post("/ingest")
 async def ingest(payload: IngestPayload, request: Request):
     now = time.time_ns()
@@ -67,10 +67,11 @@ async def ingest(payload: IngestPayload, request: Request):
     conn: asyncpg.Connection = await asyncpg.connect(os.getenv("DATABASE_URL"))
 
     try:
+        # Check if the sender is an authorized hotkey
         if not await is_authorized_hotkey(conn, signed_by):
             raise HTTPException(status_code=401, detail="Unauthorized hotkey")
         async with conn.transaction():
-            validator_request_data = payload.request
+            validator_request_data = ValidatorRequest(**json["data"]["request"])
             await conn.execute(
                 """
                 INSERT INTO validator_request (r_nanoid, block, sampling_params, ground_truth, version, hotkey) 
@@ -83,6 +84,10 @@ async def ingest(payload: IngestPayload, request: Request):
                 validator_request_data.version,
                 validator_request_data.hotkey,
             )
+            # Insert miner response data into the database
+            miner_response_data = [
+                MinerResponse(**data) for data in json["data"]["response"]
+            ]
             await conn.executemany(
                 """
                 INSERT INTO miner_response (r_nanoid, hotkey, coldkey, uid, stats) 
@@ -90,12 +95,13 @@ async def ingest(payload: IngestPayload, request: Request):
                 """,
                 [
                     (md.r_nanoid, md.hotkey, md.coldkey, md.uid, json.dumps(md.stats))
-                    for md in payload.responses
+                    for md in miner_response_data
                 ],
             )
-        return {"status": "success"}
+        return "", 200
 
     except Exception as e:
+        print(f"Error inserting miner response: {e}")
         raise HTTPException(
             status_code=500,
             detail=f"Internal Server Error: Could not insert miner response data. {str(e)}",
